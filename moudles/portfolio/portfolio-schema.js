23631e5385e9f592ff2dca6ec6c7fd87a8eaabad
alaalmasri12 <alaa_almasri1997@yahoo.com>
2020-08-16 10:27:47 +0300

    edits in schema

--- moudles/portfolio/portfolio-schema.js
+++ moudles/portfolio/portfolio-schema.js
@@ -6,7 +6,8 @@ const portfolio = mongoose.Schema({
     website : {type: String, required: true},
     image: {type: String, required: true},
     description: {type: String},
-    link:{type: String}
+    link:{type: String},
+    video:{type:String}
 });
 
 module.exports = mongoose.model('portfolio', portfolio);
