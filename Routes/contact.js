e1d4d65f4ac5549b0b87441bd3de24cdd1798fe5
alaalmasri12 <alaa_almasri1997@yahoo.com>
2020-08-21 19:11:30 +0300

    test5

--- Routes/contact.js
+++ Routes/contact.js
@@ -11,36 +11,36 @@ router.param('contact', getZip);
 router.post('/contact',handlesomething);
 
 function handlesomething(req, res) {
-    var firstname = req.body.firstname;
-    var lastname = req.body.lastname;
-    var email = req.body.email;
-    var Phone = req.body.phone;
-    var msg = req.body.message;
-    var data = {
-        "members": [{
-            email_address: email,
-            status: 'subscribed',
-            merge_fields: {
-                FNAME: firstname,
-                LNAME: lastname,
-                PHONE: Phone,
-                MMERGE5: msg
-            }
-        }],
-    }
-    var JSONdata = JSON.stringify(data);
-    console.log(JSONdata);
-    var options = {
-        url: 'https://us19.api.mailchimp.com/3.0/lists/d497c7c7f3',
-        method: 'POST',
-        headers: {
-            "Authorization": "alaa c2022d468ec18180c4be2692c07ad7e9-us19"
-        },
-        body: JSONdata
-    }
-    request(options, (error, response, body) => {
-        console.log("message has been sent");
-    });
+    // var firstname = req.body.firstname;
+    // var lastname = req.body.lastname;
+    // var email = req.body.email;
+    // var Phone = req.body.phone;
+    // var msg = req.body.message;
+    // var data = {
+    //     "members": [{
+    //         email_address: email,
+    //         status: 'subscribed',
+    //         merge_fields: {
+    //             FNAME: firstname,
+    //             LNAME: lastname,
+    //             PHONE: Phone,
+    //             MMERGE5: msg
+    //         }
+    //     }],
+    // }
+    // var JSONdata = JSON.stringify(data);
+    // console.log(JSONdata);
+    // var options = {
+    //     url: 'https://us19.api.mailchimp.com/3.0/lists/d497c7c7f3',
+    //     method: 'POST',
+    //     headers: {
+    //         "Authorization": "alaa c2022d468ec18180c4be2692c07ad7e9-us19"
+    //     },
+    //     body: JSONdata
+    // }
+    // request(options, (error, response, body) => {
+    //     console.log("message has been sent");
+    // });
     res.status(200).send('email sent');
 }
 module.exports = router;
